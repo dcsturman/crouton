9a3e5fd1be0188991e587e91c56c1ca325a9ee53
sturman <sturman@roblox.com>
2022-01-12 13:44:11 -0800

    Basic GRPC working with a simple counter.

---
+++ src/client.rs
@@ -0,0 +1,64 @@
+use crouton::catalog_client::CatalogClient;
+use crouton::QueryRequest;
+use clap::{App, Arg};
+
+pub mod crouton {
+    tonic::include_proto!("crouton");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let matches = App::new("Crouton (test) Client")
+    .version("0.1")
+    .author("Daniel Sturman <dan@sturman.org>")
+    .arg(
+        Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Port for target server.")
+            .takes_value(true),
+    )
+    .get_matches();
+
+    let port = matches.value_of("port").unwrap_or("50051");
+    let dst = format!("http://[::1]:{}", port);
+
+
+    let mut client = CatalogClient::connect(dst).await?;
+
+    let request = tonic::Request::new(QueryRequest {
+        name: "VoteCounter".into(),
+        actor: "Me".to_string(),
+    });
+
+    let response = client.create(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    let request = tonic::Request::new(QueryRequest {
+        name: "VoteCounter".into(),
+        actor: "Me".to_string(),
+    });
+
+    client.inc(request).await?;
+
+    let request = tonic::Request::new(QueryRequest {
+        name: "VoteCounter".into(),
+        actor: "Me".to_string(),
+    });
+
+    let response = client.inc(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    let request = tonic::Request::new(QueryRequest {
+        name: "VoteCounter".into(),
+        actor: "Me".to_string(),
+    });
+
+    let response = client.read(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
